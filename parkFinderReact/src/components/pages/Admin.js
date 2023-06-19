cd484c4aaddaf95027f7cdd340ca2de730aa178a
DivineWingedDemon <58912131+DivineWingedDemon@users.noreply.github.com>
2023-06-19 14:31:25 +0100

    admin programmer

--- parkFinderReact/src/components/pages/Admin.js
+++ parkFinderReact/src/components/pages/Admin.js
@@ -1,16 +1,52 @@
 import './Manager.css'
-import Filter from '../objects/Filter';
-import Navbar from '../objects/Navbar';
-import {ImageBlock} from '../interactive_items/ImageBlock';
+import NavbarStaff from '../objects/NavbarStaff';
 import { Button } from '../interactive_items/Button';
-import '../interactive_items/select.css'
+import '../pages/Details.css'
+import { useEffect, useState } from 'react';
 
-function Admin(
-    
-) {
-    <>
-    
-    </> 
+
+function Admin({
+    setIdParque
+}) {
+
+    const [selected,setSelected] = useState(1);
+    const [page,setPage] = useState(null);
+
+    function renderPage() {
+        switch (selected) {
+            default:
+                setPage(<label>{/* função/modulo para renderizar Parques (vê o discord)*/} Parques </label>);
+                break;
+        }
+    }
+
+    useEffect (() => {
+        renderPage() 
+    }, [selected]);
+
+    return (
+        <div className='staff_bg'>
+            <div className='staff_whitebox'>
+                <NavbarStaff link_logo={'/manager'}/>
+                <div className='details_options'>
+                    <Button buttonStyle={"ditails_button"+(selected===1? ' ditails_button_selected':'')} onClick={()=>{setSelected(1)}}>Parques</Button>
+                </div>
+
+                {page}
+
+                <div className='pageNumb'>
+                    <button className='page_button'> {'<<'} </button>
+                    <button className='page_button'> 1 </button>
+                    <button className='page_button'> 2 </button>
+                    <button className='page_button'> 3  </button>
+                    <button className='page_button'> {'>>'} </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Admin;
+
+
+
--- parkFinderReact/src/components/pages/Programmer.js
+++ parkFinderReact/src/components/pages/Programmer.js
@@ -1,20 +1,57 @@
 import './Manager.css'
-import Filter from '../objects/Filter';
-import Navbar from '../objects/Navbar';
-import {ImageBlock} from '../interactive_items/ImageBlock';
+import NavbarStaff from '../objects/NavbarStaff';
 import { Button } from '../interactive_items/Button';
-import '../interactive_items/select.css'
+import '../pages/Details.css'
+import { useEffect, useState } from 'react';
+
 
 function Programmer({
-    parques,
-    admins,
-    stats,
-    filter,
-    setState
+    setIdParque
 }) {
-    <>
-    
-    </> 
+
+    const [selected,setSelected] = useState(1);
+    const [page,setPage] = useState(null);
+
+    function renderPage() {
+        switch (selected) {
+            case 1:
+                setPage(<label>{/* função/modulo para renderizar Parques (vê o discord)*/} Parques </label>);
+                break;
+
+            default:
+                setPage(<label>{/* função/modulo para renderizar Administradores (vê o discord)*/} Gestores </label>);
+                break;
+        }
+    }
+
+    useEffect (() => {
+        renderPage() 
+    }, [selected]);
+
+    return (
+        <div className='staff_bg'>
+            <div className='staff_whitebox'>
+                <NavbarStaff link_logo={'/manager'}/>
+                <div className='details_options'>
+                    <Button buttonStyle={"ditails_button"+(selected===1? ' ditails_button_selected':'')} onClick={()=>{setSelected(1)}}>Parques</Button>
+                    <Button buttonStyle={"ditails_button"+(selected===2? ' ditails_button_selected':'')} onClick={()=>{setSelected(2)}}>Administradores</Button>
+                </div>
+
+                {page}
+
+                <div className='pageNumb'>
+                    <button className='page_button'> {'<<'} </button>
+                    <button className='page_button'> 1 </button>
+                    <button className='page_button'> 2 </button>
+                    <button className='page_button'> 3  </button>
+                    <button className='page_button'> {'>>'} </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Programmer;
+
+
+
