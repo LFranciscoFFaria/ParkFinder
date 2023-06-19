c4193f320fe2836a96df2bbc176ec71f3e0019e5
Casal Novo <52653136+DCasalNovo@users.noreply.github.com>
2023-06-19 11:13:18 +0100

    Update aaaaaaaaaa.js

--- parkFinderReact/src/components/pages/paginas_a_tirar/aaaaaaaaaa.js
+++ parkFinderReact/src/components/pages/paginas_a_tirar/aaaaaaaaaa.js
@@ -4,27 +4,37 @@ import '../pages/Details.css'
 import { useEffect, useState } from 'react';
 
 
-const [selected,setSelected] = useState(1);
-const [page,setPage] = useState(1);
+function funcName({
 
-function renderPage() {
-    switch (selected) {
-        case 1:
-            setPage({/* função/modulo para renderizar Pagina 1 (vê o discord)*/});
-            break;
+}) {
+    const [selected,setSelected] = useState(1);
+    const [page,setPage] = useState(1);
 
-        default:
-            setPage({/* função/modulo para renderizar Pagina 2 (vê o discord)*/});
-            break;
+    function renderPage() {
+        switch (selected) {
+            case 1:
+                setPage({/* função/modulo para renderizar Pagina 1 (vê o discord)*/});
+                break;
+
+            default:
+                setPage({/* função/modulo para renderizar Pagina 2 (vê o discord)*/});
+                break;
+        }
     }
+
+    useEffect (() => {
+        renderPage() 
+    }, [selected]);
+
+    return(
+        <div>
+            {/* ... */}
+            <div className="details_options">
+                <Button buttonStyle={"ditails_button"+(selected===1? ' ditails_button_selected':'')} onClick={()=>{setSelected(1)}}>Pagina 1</Button>
+                <Button buttonStyle={"ditails_button"+(selected===2? ' ditails_button_selected':'')} onClick={()=>{setSelected(2)}}>Pagina 2</Button>
+            </div>
+            {/* ... */}
+            {page}
+        </div>
+    );
 }
-
-useEffect (() => {
-    renderPage() 
-}, [selected]);
-
-
-<div className="details_options">
-    <Button buttonStyle={"ditails_button"+(selected===1? ' ditails_button_selected':'')} onClick={()=>{setSelected(1)}}>Pagina 1</Button>
-    <Button buttonStyle={"ditails_button"+(selected===2? ' ditails_button_selected':'')} onClick={()=>{setSelected(2)}}>Pagina 2</Button>
-</div>
